7c32b771055373aeea299949f51bfb12974ef854
kzv50d <195387848+kzv50d@users.noreply.github.com>
2026-09-10 20:35:29 -0600

    Add files via upload

--- citas.py
+++ citas.py
@@ -8,12 +8,12 @@ response = requests.get(url)
 response.encoding = 'utf-8'
 
 soup = BeautifulSoup(response.text, "html.parser")
-tabla = soup.find("div", class_="quote")
+citas = soup.find_all("div", class_="quote")
+
 datos = []
 
 if citas:
-    filas = citas.find_all("div")[1:]
-
+    for cita in citas:
         texto = cita.find("span", class_="text").text.strip()
         autor = cita.find("small", class_="author").text.strip()
         tags = ", ".join([tag.text.strip() for tag in cita.find_all("a", class_="tag")])
@@ -22,10 +22,10 @@ if citas:
             "Quote": texto,
             "Autor": autor,
             "Tags": tags
-            })
+        })
 
     df = pd.DataFrame(datos)
     df.to_csv("quotes.csv", index=False, encoding="utf-8-sig")
-    print("Scraping exitoso y archivo quote.csv creado.")
+    print("Scraping exitoso y archivo quotes.csv creado.")
 else:
-    print("No se encontró ninguna tabla en la página")
+    print("No se encontraron citas en la página")
